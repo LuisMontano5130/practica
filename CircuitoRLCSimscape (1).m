569d34177a43feb7f61a9d80c1eb4e7a3e5749f4
LuisMontano5130 <124217468+LuisMontano5130@users.noreply.github.com>
2023-05-10 09:10:31 -0600

    Add files via upload

---
+++ CircuitoRLCSimscape (1).m
@@ -0,0 +1,14 @@
+%__________________ Definicion de los parametros _______________%
+
+R = 122; % Ohmios
+L = 4.13e-3; % Henrios
+C = 0.1e-6; % Faradios
+V = 12; % Voltios
+F = 500; % Hz
+CicTra = 50; % Ciclo de trabajo del 50 por ciento
+
+%__________________ Ejecucion de simulacion _______________%
+
+open("CircuitoRLCSim.slx")
+
+% ___________________________________________________________ %
